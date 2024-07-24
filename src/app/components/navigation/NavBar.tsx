e00b3213a8b03b827856ab51e16b05d3818c2538
Rafael Palau <rafaeldonizetip@gmail.com>
2024-07-23 23:04:14 -0300

    feat: create navigation bar and add logo component

--- src/app/components/navigation/NavBar.tsx
+++ src/app/components/navigation/NavBar.tsx
@@ -1,5 +1,38 @@
+import Link from "next/link";
+import Container from "../Container";
+import Image from "next/image";
+import Logo from "../Logo/Logo";
+
+
+
 const NavBar = () => {
-  return <nav>NavBar</nav>;
+  return (
+    <nav
+      className="
+  sticky
+  top-0
+  w-full
+  bg-green-200
+  z-30
+  shadow-md
+  "
+    >
+      <div className="py-5 border-b-[2px]">
+        <Container>
+          <div className="flex justify-between items-center gap-3 md:gap-0">
+            <Link href="/">
+              <Logo />
+            </Link>
+            <div className="hidden md:block">Procurar</div>
+            <div className="flex items-center gap-6">
+              <div>Carrinho</div>
+              <div>Perfil</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
